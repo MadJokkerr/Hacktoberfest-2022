10df4357f8f03fd69c07a1bbcedd7ba5c7784ce1
Ashutosh Mohapatra <ashutosh.11907600@lpu.in>
2022-10-01 01:08:58 +0530

    Issue-3

---
+++ Programs/C++/WAP-To-Print-Fibonacci-Series.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int fibonacci_series(int n)
+{
+	if(n == 0){
+		return 0;
+	}
+	else if(n == 1){
+		return 1;
+	}
+	else{
+		return fibonacci_series(n-2) + fibonacci_series(n-1);
+	}
+}
+
+int main() {
+	int n;
+	cin>>n;
+    for(int i = 0; i < n; i++)
+    {
+        cout << fibonacci_series(i) << " ";
+    }
+	return 0;
+}
